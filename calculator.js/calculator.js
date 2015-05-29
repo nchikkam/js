f825256c2a87b0445a9202269b60b765b0c7fa48
nchikkam <narayana-babu.chikkam@here.com>
2015-05-29 16:08:45 +0200

    Simple Calculator Library to have different methods to do calculations. Idea was to experiment with node and npm for TDD.

---
+++ calculator.js/calculator.js
@@ -0,0 +1,38 @@
+;(function (global) {
+    'use strict';
+
+    var Calculator;
+
+    /*
+     * Create and return a BigNumber constructor.
+     */
+    function another() {
+        Calculator = {
+            add: function(a, b){
+                return a+ b;
+            }
+        };
+
+
+        return Calculator;
+    }
+
+
+    // EXPORT
+
+
+    Calculator = another();
+
+    // AMD.
+    if ( typeof define == 'function' && define.amd ) {
+        define( function () { return Calculator; } );
+
+    // Node and other environments that support module.exports.
+    } else if ( typeof module != 'undefined' && module.exports ) {
+        module.exports = Calculator;
+
+    // Browser.
+    } else {
+        global.Calculator = Calculator;
+    }
+})(this);
